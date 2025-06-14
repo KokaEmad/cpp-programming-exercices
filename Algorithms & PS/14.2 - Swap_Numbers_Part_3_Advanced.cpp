2906891ac36b2d9ded15ca2763856edb376ecb07
Kyrillos Emad <emadkirols90@gmail.com>
2025-06-14 11:03:00 +0300

    Add program to swap two numbers using a custom function

---
+++ Algorithms & PS/14.2 - Swap_Numbers_Part_3_Advanced.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+void readNums(double& a, double& b)
+{
+    cout << "Enter first number (a): ";
+    cin >> a;
+
+    cout << "Enter second number (b): ";
+    cin >> b;
+}
+void swapFunction(double& a , double& b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+
+void printSwap(double a, double b)
+{
+
+    cout << "\n After swapping : \n"
+        << "a = " << a << "\n"
+        << "b = " << b << "\n";
+}
+int main()
+{
+    double a, b;
+
+
+
+    readNums(a,b);
+    swapFunction(a,b);
+    printSwap(a,b);
+
+
+    return 0;
+}
+
+/*
+
+
+#include <iostream>
+#include <utility> // For std::swap
+using namespace std;
+
+int main()
+{
+    double a, b;
+
+    cout << "Enter first number (a): ";
+    cin >> a;
+
+    cout << "Enter second number (b): ";
+    cin >> b;
+
+    swap(a, b); // Standard library swap
+
+    cout << "\nAfter swapping:\n"
+         << "a = " << a << "\n"
+         << "b = " << b << "\n";
+
+    return 0;
+}
+
+*/
