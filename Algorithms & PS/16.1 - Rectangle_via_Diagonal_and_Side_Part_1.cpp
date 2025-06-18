2b3e642aa1385bec272f63a0eab58eb8e8468cb7
Kyrillos Emad <emadkirols90@gmail.com>
2025-06-18 05:41:22 +0300

    Refactor rectangle area calculation for improved structure and readability

--- Algorithms & PS/16.1 - Rectangle_via_Diagonal_and_Side_Part_1.cpp
+++ Algorithms & PS/16.1 - Rectangle_via_Diagonal_and_Side_Part_1.cpp
@@ -3,19 +3,30 @@
 
 using namespace std;
 
+void readNum(float &a , float &d )
+{
+    cin >> a;
+    cin >> d;
+}
+
 
 float calculateArea(float a, float d)
 {
     float Area = a * sqrt(pow(d, 2) - pow(a, 2));
     return Area;
 }
+
+void printArea(float area)
+{
+    cout << "Area = " << area;
+}
 int main()
 {
-    float a, d;
-    cin >> a;
-    cin >> d;
+    float side, diagonal;
+    readNum(side,diagonal);
+    ;
 
-    cout << "Area = " << calculateArea(a,d);
+    printArea(calculateArea(side, diagonal));
 
     return 0;
 }
