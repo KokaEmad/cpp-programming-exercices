53e01b649ac108de536d4672ff75b54a6bbeb867
Kyrillos Emad <emadkirols90@gmail.com>
2025-06-27 10:46:14 +0300

    Refactor and rename functions for printing letter patterns

--- Algorithms &PS L2/15 - print_Letter_Pattern.cpp
+++ Algorithms &PS L2/15 - print_Letter_Pattern.cpp
@@ -12,23 +12,27 @@ int readPositiveNumber(string message)
     return number;
 }
 
-
 void PrintLetterPattern(int Number)
 {
-    
-    for (int i = 65 + Number -1 ; i >= 65 ; i--)
+    cout << "\n";
+
+    for (int i = 65; i <= 65 + Number - 1; i++)
     {
         for (int j = 1; j <= Number - ((65 + Number - 1) - i); j++)
         {
             cout << char(i);
         }
 
-        cout << "\n"; 
+        cout << "\n";
     }
 }
 
 int main()
 {
-    PrintLetterPattern(readPositiveNumber("Enter positive number : \n"));
+   PrintLetterPattern(readPositiveNumber("Enter positive number : \n"));
+
+
+
+
 }
 
